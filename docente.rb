f61fa2a1284abd61674800148828f8541e2d5855
YalmiValentina <yalmivale@gmail.com>
2021-09-02 20:33:08 -0400

    agregar variable imprimir datos

--- docente.rb
+++ docente.rb
@@ -1,5 +1,5 @@
 class Docente
-	attr_accessor: dni,nombre,apellido,grado,antiguedad,horas_trabajadas,minutos_tardanza;
+	attr_accessor dni,nombre,apellido,grado,antiguedad,horas_trabajadas,minutos_tardanza;
 	def initialize(dni,nombre,apellido,grado,antiguedad,horas_trabajadas,minutos_tardanza)
 		@dni=dni;
 		@nombre=nombre;
@@ -9,6 +9,17 @@ class Docente
 		@horas_trabajadas=horas_trabajadas;
 		@minutos_tardanza=minutos_tardanza;
 	end
+	def imprimirDatosDocente
+		puts "Nombre #{@nombre} #{@apellido}"
+		puts "DNI #{@dni}"
+		puts "Grado de instrucción #{@grado}"
+		puts "Antiguedad #{@antiguedad}"
+		puts "Horas trabajadas #{@horas_trabajadas}"
+		puts "Minutos de tardanza #{@minutos_tardanza}"
+		puts "******************"
+	end
+
 end
 
-lala
+profesor1= Docente.new "123","juan","perez","p", "2","3","3"
+puts profesor1.dni
