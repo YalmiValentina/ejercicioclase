b8edac2ce983b22dd77bf21d261170fc767e31e7
ropema <ropema@misena.edu.co>
2021-09-02 20:40:53 -0400

    change one line

--- docente.rb
+++ docente.rb
@@ -1,14 +1,16 @@
 class Docente
-	attr_accessor: dni,nombre,apellido,grado,antiguedad,horas_trabajadas,minutos_tardanza;
-	def initialize(dni,nombre,apellido,grado,antiguedad,horas_trabajadas,minutos_tardanza)
+	attr_accessor :dni,name,apellido,grado,antiguedad,horas_trabajadas,minutos_tardanza
+	def initialize(dni,name,apellido,grado,antiguedad,horas_trabajadas,minutos_tardanza)
 		@dni=dni;
-		@nombre=nombre;
+		@name=name;
 		@apellido=apellido;
 		@grado=grado;
 		@antiguedad=antiguedad;
 		@horas_trabajadas=horas_trabajadas;
 		@minutos_tardanza=minutos_tardanza;
+		@h=10;
 	end
 end
 
-lala
+profesor1=Docente.new(432,"Juan","Alarcon",3,5,100,45);
+
